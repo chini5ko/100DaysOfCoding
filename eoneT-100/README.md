3890966bc3222a42a6ce23a4781370869a18e076
chnisko <chinisko@outlook.com>
2018-06-09 14:17:03 -0400

    adding all files

---
+++ eoneT-100/README.md
@@ -0,0 +1,3 @@
+#watch concept from the company eone
+1. Set Your timer for 100 days of coding (inside the "timer.js" file)
+2. A 1 hour code of timer, Just press the button "Start 1 hour code"
---
+++ eoneT-100/timer.js
@@ -0,0 +1,98 @@
+// Set the date when your 100 days of coding is done ! 
+var countDownDate = new Date("Sep 17, 2018 12:00:00").getTime();
+
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+ 
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "Done!";
+    }
+}, 1000);
+
+var x = interval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+ 
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = "challege: days " + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "Done!";
+    }
+}, 1000);
+
+function myFunction() {
+   
+    //************************************************************ */
+    var codingTimeMinute = 60; //change this variable for the timer
+    var codingTimeSeconds = codingTimeMinute * 60;//1 hour
+
+    var showMinute = Math.floor(codingTimeSeconds/60);
+    var showSeconds =60;
+
+    setInterval(function(){  
+    codingTimeSeconds-=1;
+    showMinute = Math.floor(codingTimeSeconds/60);
+    
+    if(codingTimeSeconds>0){
+        console.log("coding time secodns is more");
+        if(showSeconds==0){
+            showSeconds=60;
+        }
+        showSeconds-=1;
+    }
+    if(codingTimeSeconds==0){
+        alert("One hour completed!"); 
+        codingTimeSeconds=-1;
+    }
+
+    //html print
+    if(codingTimeSeconds>0){
+        document.getElementById("hourTimer").innerHTML = "Time of coding left Today:"
+       +showMinute+"m " + showSeconds+ "s"; 
+    }
+
+    if(codingTimeSeconds<0){
+        document.getElementById("hourTimer").innerHTML = "You are going extra!"
+    }
+      
+    }, 1000);
+}
+
+        
