99d14b0c87c530efb56aa1d1306003a2175fc1ac
chnisko <chinisko@outlook.com>
2018-06-20 17:58:44 -0400

    syncronize

--- 100DaysWeb/scriptFB.js
+++ 100DaysWeb/scriptFB.js
@@ -1,21 +1,54 @@
+//data
+var mehmetLink = "@mehmet";
+//code description
+
+
 
 //****Button********//
 function getPost() {
+    var code = document.getElementById("codeDescription").value;
+    var why = document.getElementById("why").value;
+    var language = document.getElementById("language").value;
+    var githubLink = document.getElementById("githubLink").value;
+
+    //console.log(" , " + why + code + " " + language + " " + githubLink);
+
     //rotate the image
    // rotate();
-
     //get line values of the lines
-    var challenger = document.getElementById("option-1").value;
+    var challengerDays;
+    if(document.getElementById("option-0").checked){
+        challengerDays = document.getElementById("option-0").value;
+    }
+    if(document.getElementById("option-1").checked){
+        challengerDays = document.getElementById("option-1").value;
+    }
+    if(document.getElementById("option-2").checked){
+        challengerDays = document.getElementById("option-2").value;
+    }
+    if(document.getElementById("option-3").checked){
+        challengerDays = document.getElementById("option-3").value;
+    }
+    //facebook post for github 100 days of Code
+    var facebookPost = "#100DaysOfCode Day " + days[challengerDays]; 
+    facebookPost+= " with Mehmet, Momo, Eddie. <br> " ;
+    facebookPost+= "Code Description: " + "<br> \n" ;
+    facebookPost+= code + " <br>";
+    facebookPost+= "Why : " + "<br>" ;
+    facebookPost+= why + " <br>";
+    facebookPost+= "Language: "  ;
+    facebookPost+= language + " <br> " + mehmetLink;
+    facebookPost+= "Github Link: ";
+    facebookPost+= githubLink;;
+    //facebookPost+= code;
   
+    console.log(facebookPost);
 
-    var answer = challenger;
-    console.log(answer);
-
-    if (challenger.length == 0) {
+    if (challengerDays.length == -1) {
         window.alert("Select an option");
     }
     else {
-        document.getElementById("showResult").innerHTML = challenger;
+        document.getElementById("showResult").innerHTML = facebookPost;
     }
 }
 
