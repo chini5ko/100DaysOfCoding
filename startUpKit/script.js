982d331b1505bf63a0c24cf2ca1424fe7c27d136
chnisko <chinisko@outlook.com>
2018-06-26 17:34:19 -0400

    profit function added in Financial

--- startUpKit/script.js
+++ startUpKit/script.js
@@ -2,3 +2,21 @@
     this.MaterialProgress.setProgress(50);
     this.MaterialProgress.setBuffer(5);
   });
+
+  var grossProfit = 0;
+  var revenue = 0;
+  var cost = 0;
+
+  function save(){
+
+    revenue = document.getElementById("revenue").value;
+    cost = document.getElementById("cost").value;
+
+    console.log(revenue);
+    console.log(cost);
+
+    grossProfit = revenue-cost;
+
+    document.getElementById("show").innerHTML = grossProfit;
+
+  }
