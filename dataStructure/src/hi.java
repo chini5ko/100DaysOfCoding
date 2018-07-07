3a8ed5fab5d562e1f62104b2cf5285d9ed58d962
chnisko <chinisko@outlook.com>
2018-07-06 23:23:36 -0400

    google Tutorial material design

---
+++ dataStructure/src/hi.java
@@ -0,0 +1,15 @@
+
+public class hi {
+    public static void main(String[] args) {
+            System.out.println("First Day");
+
+            //limitation of Arrays
+            //limited amount of data
+            //slow operation, no insertion
+            //too small, cant add more mumbers
+            //too large, waste of space
+    }
+}
+
+
+
