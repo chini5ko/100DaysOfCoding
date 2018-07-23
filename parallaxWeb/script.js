098eb2e86e02e429a67088ec63120542417a08f4
chnisko <chinisko@outlook.com>
2018-07-23 12:47:40 -0400

    added tabs for time description feature

--- parallaxWeb/script.js
+++ parallaxWeb/script.js
@@ -26,3 +26,18 @@ function toggleFunction() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+
+//Tabs for time descritpion 
+function openCity(evt, cityName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+       tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.firstElementChild.className += " w3-border-red";
+  }
