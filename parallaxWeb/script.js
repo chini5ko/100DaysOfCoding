8d516a73c4f631e67a3af564a11fec3952e10d35
chnisko <chinisko@outlook.com>
2018-07-23 23:39:39 -0400

    list

--- parallaxWeb/script.js
+++ parallaxWeb/script.js
@@ -36,8 +36,8 @@ function openCity(evt, cityName) {
     }
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < x.length; i++) {
-       tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+       tablinks[i].className = tablinks[i].className.replace(" w3-border-blue", "");
     }
     document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.firstElementChild.className += " w3-border-red";
+    evt.currentTarget.firstElementChild.className += " w3-border-blue";
   }
