77e03b690f46c2f5445a41291b6302f0adf22e73
chnisko <chinisko@outlook.com>
2018-07-25 23:15:55 -0400

    passed 2 levels of code challenge

--- parallaxWeb/script.js
+++ parallaxWeb/script.js
@@ -1,6 +1,6 @@
 // Modal Image Gallery
 function onClick(element) {
-  document.getElementById("img01").src = element.src;
+  document.getElementyId("img01").src = element.src;
   document.getElementById("modal01").style.display = "block";
   var captionText = document.getElementById("caption");
   captionText.innerHTML = element.alt;
